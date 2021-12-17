bfcadb2f34017b089d9b91cb97f26a287ce5d596
Kaushik <rknarayan02@gmail.com>
2021-12-17 15:34:36 +0530

    Modified SQL setup file for Heroku

--- init.sql
+++ init.sql
@@ -1,10 +1,3 @@
-\c postgres;
-DROP DATABASE IF EXISTS userprod_api;
-CREATE DATABASE userprod_api;
-\c userprod_api;
-CREATE SCHEMA IF NOT EXISTS product_db_schema;
-ALTER DATABASE userprod_api SET search_path TO userprod_api_schema, public;
-
 CREATE SEQUENCE IF NOT EXISTS products_pk_seq START 20000;
 
 CREATE TABLE IF NOT EXISTS users (
